b8b0b2922c5f21c1dd5d8cd5691a3f176e49073c
Wouter Bolsterlee <uws@xs4all.nl>
2015-07-11 23:14:35 +0200

    Actually include the module in the release dist

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name='argskwargs',
-    version='0.0.1',
+    version='0.0.2',
     author='Wouter Bolsterlee',
     author_email='Wouter Bolsterlee',
+    py_modules=['argskwargs'],
 )
