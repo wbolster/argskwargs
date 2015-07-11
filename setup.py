77a11c5512d7e98b5a54836f3279e648d300d345
Wouter Bolsterlee <uws@xs4all.nl>
2015-07-11 23:25:53 +0200

    Expand setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
+import os
 from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
 
 setup(
     name='argskwargs',
-    version='0.0.2',
+    description="flexible container for positional and keyword arguments",
+    long_description=long_description,
+    version='0.0.3',
     author='Wouter Bolsterlee',
     author_email='Wouter Bolsterlee',
+    url="https://github.com/wbolster/argskwargs",
     py_modules=['argskwargs'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
